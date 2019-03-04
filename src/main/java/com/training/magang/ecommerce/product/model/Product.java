fd1a5ffa806ea5e11932d83541e1d0abbabc8b6a
karnandohuang <karnandohuang@outlook.com>
2019-03-04 16:58:39 +0700

    COMMIT-1

---
+++ src/main/java/com/training/magang/ecommerce/product/model/Product.java
@@ -0,0 +1,70 @@
+package com.training.magang.ecommerce.product.model;
+
+public class Product {
+
+    private int id;
+    private String name;
+    private String price;
+    private String sku;
+    private int categoryId;
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    private String category;
+    private int weight;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
