476e825875f8214da617afc4104dc2f28dec82fb
karnandohuang <karnandohuang@outlook.com>
2019-03-13 15:42:05 +0700

    COMMIT-5 Added API Key Error 401 & Kafka

--- src/main/java/com/training/magang/ecommerce/product/Application.java
+++ src/main/java/com/training/magang/ecommerce/product/Application.java
@@ -1,10 +1,20 @@
 package com.training.magang.ecommerce.product;
 
+import com.training.magang.ecommerce.product.model.ApiKeyHandlerMethodArgumentResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @SpringBootApplication
-public class Application {
+public class Application implements WebMvcConfigurer {
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(new ApiKeyHandlerMethodArgumentResolver());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
